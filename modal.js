8da96ae76ca2128d12a5ae2ba1968550400c0e41
moshe.frydmann <moshe.frydmann@gmail.com>
2021-06-27 22:41:00 +0300

    validation réusie

--- modal.js
+++ modal.js
@@ -11,6 +11,26 @@ function editNav() {
   const modalbg = document.querySelector(".bground");
   const modalBtn = document.querySelectorAll(".modal-btn");
   const formData = document.querySelectorAll(".formData");
+  const submit = document.getElementById("form-container");
+  const checkInputs = document.querySelectorAll('.check-input');
+
+  submit.addEventListener('submit', () => {
+
+    let count = 0;
+
+    for (let i = 0; i < checkInputs.length; i++) {
+        if (checkInputs[i].value != '') {
+          count++;
+        }
+    }
+
+    console.log("count = " + count);
+
+    if (count == 6) {
+
+      alert("Merci ! Votre réservation a été reçue.");
+    }
+  });
   
   // launch modal event
   modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
